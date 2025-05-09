c5ee43c7fccdb670bba072c3c2810a0c168000d5
Arb-Lim <arkleonpro@gmail.com>
2025-05-08 23:26:06 -1000

    cleaned up styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { CssBaseline, Container, Typography } from "@mui/material";
+import {
+  CssBaseline,
+  Container,
+  Typography,
+  GlobalStyles,
+} from "@mui/material";
 import TextExpander from "./components/TextExpander";
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <GlobalStyles
+        styles={{
+          body: {
+            margin: 0,
+            fontFamily: '"Roboto", sans-serif',
+          },
+        }}
+      />
       <Container maxWidth="md" sx={{ pt: 4 }}>
         <Typography variant="h4" component="h1" textAlign="center" gutterBottom>
-          Regex-Based RN Shorcuts Expander
+          Regex-Based RN Shortcuts Expander
         </Typography>
         <TextExpander />
       </Container>
